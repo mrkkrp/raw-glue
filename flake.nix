ac8820bdc24d0726b20719992111314521290c40
Mark Karpov <markkarpov92@gmail.com>
2023-02-25 21:20:26 +0100

    Make the latest version of libraw available

--- flake.nix
+++ flake.nix
@@ -15,57 +15,65 @@
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem (system:
-      let overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
-            extensions = [
-              "rust-src"
-              "rust-analysis"
-              "rustfmt"
-              "clippy"
-            ];
-          };
-          craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
-          src = craneLib.cleanCargoSource ./.;
-          commonArgs = { inherit src; };
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          my-crate = craneLib.buildPackage (commonArgs // {
-            inherit cargoArtifacts;
-          });
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+          extensions = [
+            "rust-src"
+            "rust-analysis"
+            "rustfmt"
+            "clippy"
+          ];
+        };
+        craneLib = crane.lib.${system}.overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource ./.;
+        sharedBuildInputs = [
+          pkgs.libraw.dev
+        ];
+        sharedNativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+        commonArgs = {
+          inherit src;
+          buildInputs = sharedBuildInputs;
+          nativeBuildInputs = sharedNativeBuildInputs;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
-        {
+      {
         checks = {
-
           my-crate-clippy = craneLib.cargoClippy (commonArgs // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           });
-
           my-crate-doc = craneLib.cargoDoc (commonArgs // {
             inherit cargoArtifacts;
           });
-
           my-crate-fmt = craneLib.cargoFmt {
             inherit src;
           };
-
-          };
-          packages.default = my-crate;
-          apps.default = flake-utils.lib.mkApp {
-            drv = my-crate;
-          };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = builtins.attrValues self.checks;
-            nativeBuildInputs = with pkgs; [
-              cargo
-              clippy
-              rust-analyzer
-              rustc
-              rustfmt
-            ];
-          };
-        }
+        };
+        packages.default = my-crate;
+        apps.default = flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+          buildInputs = sharedBuildInputs;
+          nativeBuildInputs = [
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rust-analyzer
+            pkgs.rustc
+            pkgs.rustfmt
+          ] ++ sharedNativeBuildInputs;
+        };
+      }
     );
 }
