1a922af67a8dbe418b68e90244d1c5a062e53675
Mark Karpov <markkarpov92@gmail.com>
2023-02-25 21:25:51 +0100

    Add a very approximate plan of what I want to do

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // 0. parse command line options
+
+    // 1. use libraw to load all specified raw files
+
+    // 2. use libpano to establish mapping between the loaded raw files
+
+    // 3. determine how to crop (first iteration we can do without)
+
+    // 4. stitch / crop and save as a tiff file
+
     println!("Hello, world!");
 }
